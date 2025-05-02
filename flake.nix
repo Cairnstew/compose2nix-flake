1153d18589210004bae9cf9357eaf1ac061ffc4f
seancairns14 <sean.cairnsst@gmail.com>
2025-05-02 17:40:13 -0500

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -6,36 +6,39 @@
     compose2nix.url = "github:aksiksi/compose2nix";
   };
 
-  outputs = { self, nixpkgs, compose2nix, ... }@inputs:
-    let
-      pkgs = import nixpkgs { system = "x86_64-linux"; };
-      compose2nixPkg = compose2nix.packages.x86_64-linux.default;
-      compose-files = [
-        # Add more files here
-      ];
-      
-      compose-files-str = builtins.concatStringsSep "," (builtins.map (path: toString path) compose-files);
-    in
-    {
-      packages.x86_64-linux.default = pkgs.writeShellApplication {
-        name = "run-compose2nix";
-        runtimeInputs = [ compose2nixPkg ];
-        text = ''
-          set -e
+  outputs = { self, nixpkgs, compose2nix, ... }@inputs: {
+    perSystem = { self', system, pkgs, ... }:
+      let
+        compose2nixPkg = compose2nix.packages.${system}.default;
+        compose-files = [
+          # Add more files here
+        ];
 
-          echo "Running compose2nix with inputs: ${compose-files-str}"
+        compose-files-str = builtins.concatStringsSep "," (builtins.map (path: toString path) compose-files);
+      in {
+        packages.default = pkgs.writeShellApplication {
+          name = "run-compose2nix";
+          runtimeInputs = [ compose2nixPkg ];
+          text = ''
+            set -e
 
-          compose2nix \
-            -inputs="${compose-files-str}" \
-            -project="home-manager" \
-            -runtime="docker" \
-            -output="/etc/nixos/apps/imports/docker-containers.nix"
-        '';
+            echo "Running compose2nix with inputs: ${compose-files-str}"
+
+            compose2nix \
+              -inputs="${compose-files-str}" \
+              -project="home-manager" \
+              -runtime="docker" \
+              -output="../../modules/nixos/common/docker-containers.nix"
+          '';
+        };
+
+        apps.default = {
+          type = "app";
+          program = "${self'.packages.default}/bin/run-compose2nix";
+        };
+
+        # Optional: for nix fmt
+        formatter = pkgs.nixpkgs-fmt;
       };
-
-      apps.x86_64-linux.default = {
-        type = "app";
-        program = "${self.packages.x86_64-linux.default}/bin/run-compose2nix";
-      };
-    };
+  };
 }
